dbc6948e593dc4eda583ae53898a6c601a60c87d
jiaomingyu <jiaomingyu5778@gmail.com>
2020-01-20 15:19:46 +0800

    第一次提交

--- src/test/java/com/atguigu/es/demo/EsDemoApplicationTests.java
+++ src/test/java/com/atguigu/es/demo/EsDemoApplicationTests.java
@@ -2,10 +2,18 @@ package com.atguigu.es.demo;
 
 import com.atguigu.es.demo.pojo.User;
 import com.atguigu.es.demo.repository.UserRepository;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
+import org.elasticsearch.search.sort.SortBuilders;
+import org.elasticsearch.search.sort.SortOrder;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 
 import java.util.Arrays;
 import java.util.List;
@@ -48,5 +56,24 @@ class EsDemoApplicationTests {
     void findByAge(){
         this.repository.findByAgeBetween(22,25).forEach(System.out::println);
         this.repository.findByName("二锤").forEach(System.out::println);
+    }
+
+    @Test
+    void testNativeQuery(){
+        // 自定义查询
+        NativeSearchQueryBuilder queryBuilder = new NativeSearchQueryBuilder();
+        // 构建查询
+        queryBuilder.withQuery(QueryBuilders.matchQuery("name", "张大锤"));
+        // 分页查询
+        queryBuilder.withPageable(PageRequest.of(0, 2));
+        // 排序
+        queryBuilder.withSort(SortBuilders.fieldSort("age").order(SortOrder.DESC));
+        // 构建高亮
+        queryBuilder.withHighlightBuilder(new HighlightBuilder().field("name").preTags("<em>").postTags("</em>"));
+
+        Page<User> userPage = this.repository.search(queryBuilder.build());
+        System.out.println("命中数：" + userPage.getTotalElements());
+        System.out.println("总页数:" + userPage.getTotalPages());
+        userPage.getContent().forEach(System.out::println);
     }
 }
